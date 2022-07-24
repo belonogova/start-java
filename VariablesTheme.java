e3bd7425da1c647e2d9c26feb442b76f38d3711b
BelonogovaEA <deshkina_elena@bk.ru>
2022-07-24 20:01:11 +0300

    Add Lesson 1 (Variables)

---
+++ VariablesTheme.java
@@ -0,0 +1,27 @@
+public class VariablesTheme {
+	public static void main(String[] args) {
+	System.out.println("1. Создание переменных и вывод их значений на консоль\n");
+
+	byte core = 64;
+	char release = 'W';
+	int memory = 32;
+	long codProduct = 330800000000L;
+	short vertion = 21;
+	float assemblyOC = 19044.1826f;
+	double contact = 120.22124180d;
+	boolean isTrue = true;
+
+	System.out.println(release);
+	System.out.println(core);
+	System.out.println(memory);
+	System.out.println(codProduct);
+	System.out.println(vertion);
+	System.out.println(assemblyOC);
+	System.out.println(contact);
+	System.out.println(isTrue);
+
+	System.out.println("\n\n2. Расчет стоимости товара со скидкой\n");
+
+
+    }
+}
