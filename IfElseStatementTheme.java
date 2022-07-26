c60254c89c7c245f3ac5f485bf9c02980f3bdd72
BelonogovaEA <deshkina.ale@gmail.com>
2022-07-26 17:24:14 +0300

    Lesson 1 (IfElseStatementTheme) Task 3, 4

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -7,7 +7,7 @@
         boolean sex = false; 
         String name = "Alena";
 
-        if(age > 20) {
+        if (age > 20) {
             System.out.println("Я уже взрослая!");
         } else {
             System.out.println("Я еще малеькая!");
@@ -17,7 +17,7 @@
             System.out.println("Я женщина!");
         }
 
-        if(height < 1.80) {
+        if (height < 1.80) {
             System.out.println("Нужно еще подрасти!");
         } else {
             System.out.println("Могу достать воробушка");
@@ -25,9 +25,9 @@
 
         char firstLetter = name.charAt(0);
 
-        if(firstLetter == 'L') {
+        if (firstLetter == 'L') {
             System.out.println("Тебя зовут Лена?");
-        } else if(firstLetter == 'E') {
+        } else if (firstLetter == 'E') {
             System.out.println("Тебя зовут Елена?");
         } else {
             System.out.println("Значит тебя зовут Алена!");
@@ -38,7 +38,7 @@
         int num1 = 5;
         int num2 = 7;
 
-        if(num1 < num2) {
+        if (num1 < num2) {
             System.out.println("max - " + num2 +
                 "\nmin - " + num1);
         } else {
@@ -48,6 +48,65 @@
 
         System.out.println("\n\n 3. Работа с числом \n");
 
+        int num = 9;
 
-     }
+        if (num == 0) {
+
+        } 
+
+        if (num % 2 == 0) {
+            System.out.println("Число " + num + " четное");
+        } else {
+            System.out.println(" Число " + num + " нечетное");
+        }
+
+        if (num > 0) {
+            System.out.println("Число " + num + " положительное");
+        } else {
+            System.out.println("Число " + num + " отрицательное");
+        }
+
+        System.out.println("\n\n 4. Поиск одинаковых цифр в числах \n");
+
+        int numb1 = 235;
+        int numb2 = 735;
+
+        String numb1String = numb1 + "";
+        String numb2String = numb2 + "";
+
+        System.out.println("Число первое - " + numb1 +
+            "\nЧисло второе - " + numb2);
+
+        System.out.println("Одинаковые цифры - ");
+
+        if (numb1String.charAt(0) == numb2String.charAt(0)) {
+            System.out.println(numb1String.charAt(0) + "");
+        }
+
+        if (numb1String.charAt(1) == numb2String.charAt(1)) {
+            System.out.println(numb1String.charAt(1) + "");
+        }
+
+        if (numb1String.charAt(2) == numb2String.charAt(2)) {
+            System.out.println(numb1String.charAt(2));
+        }
+
+        System.out.println("Номера разрядов");
+
+        if (numb1String.charAt(0) == numb2String.charAt(0)) {
+            System.out.println(3);
+        }
+
+        if (numb1String.charAt(1) == numb2String.charAt(1)) {
+            System.out.println(2);
+        }
+
+        if (numb1String.charAt(2) == numb2String.charAt(2)) {
+            System.out.println(1);
+        }
+
+        System.out.println("\n\n 5. Определение буквы, числа или символа по их коду \n");
+
+        char  who = '\u0057';
+    }
 }
