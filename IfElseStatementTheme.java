0cb779aa6ffeb5778e0aa20dcce698f922e18a35
BelonogovaEA <deshkina.ale@gmail.com>
2022-07-26 14:35:30 +0300

    Lesson 1 (IfElseStatementTheme) Task 1, 2

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -1,53 +1,53 @@
  public class IfElseStatementTheme {
-	 public static void main(String[] args) {
-	 	System.out.println("\n 1. Перевод псевдокода на язык Java \n");
+     public static void main(String[] args) {
+        System.out.println("\n 1. Перевод псевдокода на язык Java \n");
 
-	 	int age = 33;
-	 	double  height = 1.60;
-	 	boolean sex = false; 
-	 	String name = "Alena";
+        int age = 33;
+        double  height = 1.60;
+        boolean sex = false; 
+        String name = "Alena";
 
-	 	if(age > 20) {
-	 		System.out.println("Я уже взрослая!");
-	 	} else {
-	 		System.out.println("Я еще малеькая!");
-	 	}
+        if(age > 20) {
+            System.out.println("Я уже взрослая!");
+        } else {
+            System.out.println("Я еще малеькая!");
+        }
 
-	 	if (sex != true) {
-	 		System.out.println("Я женщина!");
-	 	}
+        if (sex != true) {
+            System.out.println("Я женщина!");
+        }
 
-	 	if(height < 1.80) {
-	 		System.out.println("Нужно еще подрасти!");
-	 	} else {
-	 		System.out.println("Могу достать воробушка");
-	 	}
+        if(height < 1.80) {
+            System.out.println("Нужно еще подрасти!");
+        } else {
+            System.out.println("Могу достать воробушка");
+        }
 
-	 	char firstLetter = name.charAt(0);
+        char firstLetter = name.charAt(0);
 
-	 	if(firstLetter == 'L') {
-	 		System.out.println("Тебя зовут Лена?");
-	 	} else if(firstLetter == 'E') {
-	 		System.out.println("Тебя зовут Елена?");
-	 	} else {
-	 		System.out.println("Значит тебя зовут Алена!");
-	 	}
+        if(firstLetter == 'L') {
+            System.out.println("Тебя зовут Лена?");
+        } else if(firstLetter == 'E') {
+            System.out.println("Тебя зовут Елена?");
+        } else {
+            System.out.println("Значит тебя зовут Алена!");
+        }
 
-	 	System.out.println("\n\n 2. Поиск max и min числа \n");
+        System.out.println("\n\n 2. Поиск max и min числа \n");
 
-	 	int num1 = 5;
-	 	int num2 = 7;
+        int num1 = 5;
+        int num2 = 7;
 
-	 	if(num1 < num2) {
-	 		System.out.println("max - " + num2 +
-	 			"\nmin - " + num1);
-	 	} else {
-	 		System.out.println("max - " + num1 +
-	 			"\nmin - " + num2);
-	 	}
+        if(num1 < num2) {
+            System.out.println("max - " + num2 +
+                "\nmin - " + num1);
+        } else {
+            System.out.println("max - " + num1 +
+                "\nmin - " + num2);
+        }
 
-	 	System.out.println("\n\n 3. Работа с числом \n");
+        System.out.println("\n\n 3. Работа с числом \n");
 
 
-	 }
+     }
 }
