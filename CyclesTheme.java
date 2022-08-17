d6d23d937fca921990e87a82e298bcf5b710463d
BelonogovaEA <deshkina.ale@gmail.com>
2022-08-17 09:47:24 +0300

    Lesson 1 (CyclesTheme) Task 4, 5, 6

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -46,15 +46,86 @@ public class  CyclesTheme {
         System.out.println("\n\n 3. Вывод реверсивного числа и суммы его цифр \n");
 
         int number = 1234;
-        int i = 0;
+        int numb = 0;
         int sum = 0;
 
         while (number > 0) {
             sum = sum + number % 10;
-            i = number % 10;
+            numb = number % 10;
             number = number / 10;
-            System.out.print(i);
+            System.out.print(numb);
             }
-        System.out.println("\nСумма цифр: " + sum);
+        System.out.println("Сумма цифр: " + sum);
+
+        System.out.println("\n\n 4. Вывод чисел на консоль в несколько строк \n");
+
+        for (int i = 1; i < 24 ; i += 2) {
+            if (i % 5 == 0) {
+                System.out.println();
+            }
+            System.out.printf(" %d", i);
+        }
+
+        System.out.println("\n\n 5. Проверка количества единиц на четность \n");
+
+
+        int numbers = 3141591;
+        int count = 0;
+        int numbers3141591 = numbers;
+
+        while (numbers > 0) {
+            int number1 = numbers % 10;
+            if (number1 == 1) {
+                count++;
+            }
+            numbers = numbers / 10;
+        }
+            
+        if (count % 2 == 0) {
+            System.out.println("Число " + numbers3141591 + " содержит " + count + " четное количество единиц");
+        }else {
+             System.out.println("Число " + numbers3141591 + " содержит " + count + " нечетное количество единиц");
+        }
+
+        System.out.println("\n\n 6. Отображение фигур в консоли \n");
+
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        int i = 5;
+        while (i > 0) {
+            int j = i;
+            while (j > 0) {
+                System.out.print("#");
+                j--;
+            }
+            System.out.println();
+            i--;
+        }
+        i = 2;
+        do {
+            int j = i;
+            while (j < 3 ) {
+                System.out.print("$");
+                j++;
+            }
+            System.out.println();
+            i--;
+        } while (i > -1);
+        i = 0;
+        do {
+            int j = i;
+            while (j < 2 ) {
+                System.out.print("$");
+                j++;
+            }
+            System.out.println();
+            i++;
+        } while (i < 3);
+
+        System.out.println("\n\n 7. Отображение ASCII-символов \n");
     }
 }
