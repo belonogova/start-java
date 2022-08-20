e3203df7a2483acf83a59a6259ca57bca1c65a10
BelonogovaEA <deshkina.ale@gmail.com>
2022-08-20 13:25:25 +0300

    Lesson 1 (FindNumber)

---
+++ FindNumber.java
@@ -0,0 +1,21 @@
+public class FindNumber {
+    public static void main(String[] args) {
+        int num = 40;
+        int number = 65;
+
+        do {
+            if (number > 0 && number <= 100) {
+                if (number < num) {
+                    System.out.println("Число меньше загаданного");
+                    number += 5;
+                } else {
+                    System.out.println("Число больше загаданного");
+                    number -= 5;
+                }
+            } else {
+                System.out.println("Число не в диапазоне");
+            }
+        } while (num != number);
+            System.out.println("Вы победили!");
+    }
+}
