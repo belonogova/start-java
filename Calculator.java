b693dae3050f8962d056421c32d4c5bb0ca9887f
BelonogovaEA <deshkina.ale@gmail.com>
2022-08-23 10:03:42 +0300

    Lesson 1 (Calculator) bugfix

--- Calculator.java
+++ Calculator.java
@@ -3,28 +3,23 @@ public class Calculator {
         int num1 = 2;
         int num2 = 3;
         char sign = '^';
+        int result = 0;
         if (sign == '+') {
-            int result = num1 + num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+            result = num1 + num2;
         } else if (sign == '-') {
-            int result = num1 - num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+            result = num1 - num2;
         } else if (sign == '*') {
-            int result = num1 * num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+            result = num1 * num2;
         } else if (sign == '/') {
-            int result = num1 / num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+            result = num1 / num2;
         } else if (sign == '%') {
-            int result = num1 % num2;
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+            result = num1 % num2;
         } else if (sign == '^') { 
-            int result = 1;
+            result = 1;
             for (int i = num2; i > 0; i--) {
                 result *= num1;
             }
-            System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
         }
-
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
     }
 }
