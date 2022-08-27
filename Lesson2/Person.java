24c8923d5d3ae5d06e1f67d1da210521f12085fe
BelonogovaEA <deshkina.ale@gmail.com>
2022-08-27 19:02:58 +0300

    Lesson 2 (Person)

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Andrei";
+    String sex = "Man";
+    int height = 187;
+    int weight = 100;
+    int age = 38;
+
+    void go() {
+        System.out.println("going");
+    }
+
+    void sit() {
+        System.out.println("sitting");
+    }
+
+    void run() {
+        System.out.println("runing");
+    }
+
+    void talk() {
+        System.out.println("talking");
+    }
+
+    void learnJava() {
+        System.out.println("learning Java");
+    }
+}
