98809a6cbb24de804f8886e3d4258559b13989c7
BelonogovaEA <deshkina.ale@gmail.com>
2022-08-27 19:52:07 +0300

    Lesson 2 (WolfTest)

---
+++ Lesson2/WolfTest.java
@@ -0,0 +1,22 @@
+public class WolfTest {
+    public static void main(String[] args) {
+
+        Wolf wolfOne = new Wolf();
+
+        wolfOne.sex = "Men";
+        wolfOne.name = "Friend";
+        wolfOne.age = 3;
+        wolfOne.color = "grey";
+
+        System.out.println("Пол = " + wolfOne.sex);
+        System.out.println("Имя = " + wolfOne.name);
+        System.out.println("Возраст = " + wolfOne.age);
+        System.out.println("Окрас = " + wolfOne.color);
+
+        wolfOne.go();
+        wolfOne.sit();
+        wolfOne.run();
+        wolfOne.howl();
+        wolfOne.hunt();
+    }
+}
