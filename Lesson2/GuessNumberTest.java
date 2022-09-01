92efedb65ae6121d493b70ad83fdccdc3a741a63
BelonogovaEA <deshkina.ale@gmail.com>
2022-09-01 16:13:13 +0300

    Lesson2 (GuessNumber)

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -16,25 +16,28 @@ public class GuessNumberTest {
 
         do {
             int numGenerate = GuessNumber.generateNumber();
-            int numPlayer1 = player1.guess();
-            int numPlayer2 = player2.guess();
+            int numPlayer1 = 0;
+            int numPlayer2 = 0;
             while (numGenerate != numPlayer1 || numGenerate != numPlayer2) {
+                numPlayer1 = player1.guess();
                 if (numPlayer1 > numGenerate) {
                     System.out.println("Число " + numPlayer1 + " больше того, что загадал компьютер");
                 } else if (numPlayer1 < numGenerate) {
                     System.out.println("Число " + numPlayer1 + " меньше того, что загадал компьютер");
+                } else {
+                    System.out.println("Вы выиграли! Загаданное число: " + numGenerate);
+                    break;
                 }
-                
+                numPlayer2 = player2.guess();
                 if (numPlayer2 > numGenerate) {
                     System.out.println("Число " + numPlayer2 + " больше того, что загадал компьютер");
                 } else if (numPlayer2 < numGenerate) {
                     System.out.println("Число " + numPlayer2 + " меньше того, что загадал компьютер");
+                } else {
+                    System.out.println("Вы выиграли! Загаданное число: " + numGenerate);
+                    break;
                 }
-
-                numPlayer1 = player1.guess();
-                numPlayer2 = player2.guess();
             }
-            System.out.println("Вы выиграли! Загаданное число: " + numGenerate);
             do {
                 System.out.println("Хотите продолжить игру? [yes/no]:");
                 answer = scanner.nextLine();
